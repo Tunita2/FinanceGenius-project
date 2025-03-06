fb2cb185766b236e827125b0918f7a436e834992
NguyenTrunHieu3 <hieugrth13@gmail.com>
2025-03-06 14:14:00 +0700

    add update and edit function

--- src/controllers/reminderController.js
+++ src/controllers/reminderController.js
@@ -1,13 +1,12 @@
 const connection = require('../config/database')
-const getViewReminders = (req, res) => {
-    try{
-        res.render('viewReminders.ejs',{
-            
-        })
-    }catch(error){
+const getViewReminders = async (req, res) => {
+    try {
+        let [results, fields] = await connection.query('SELECT * FROM Reminders');
+        return res.render('viewReminders.ejs', {listReminder: results});
+    } catch (error) {
         console.error(error)
-        res.status(500).send("ERROR HAPPEND!")
-}
+        res.send("ERROR HAPPEND!")
+    }
 }   
 const getCreateReminder = (req, res) => {
     try{
@@ -16,7 +15,7 @@ const getCreateReminder = (req, res) => {
         })
     }catch(error){
         console.error(error)
-        res.status(500).send("ERROR HAPPEND!")
+        res.send("ERROR HAPPEND!")
 }
 }   
 const getEditReminder = (req, res) => {
@@ -26,13 +25,83 @@ const getEditReminder = (req, res) => {
         })
     }catch(error){
         console.error(error)
-        res.status(500).send("ERROR HAPPEND!")
+        res.send("ERROR HAPPEND!")
 }
-} 
+}
+ 
+const postCreateReminder = async (req, res) => {
+    try{
+        let name = req.body.reminderName;
+        let amount = req.body.amount;
+        let dueDate = req.body.dueDate;
+        let frequency = req.body.frequency;
+        let reminderMethod = req.body.reminderMethod;
+        let reminderNotification = req.body.reminderNotification;
 
+        let [results, fields] = await connection.query(`INSERT INTO Reminders (payment_account_name, amount, due_date, frequency, payment_method, notification_time) VALUES (?,?,?,?,?,?)`, [name, amount, dueDate, frequency, reminderMethod, reminderNotification]);
+        res.redirect('/view-reminders');
+    }catch(error){
+        console.error(error)
+        res.send("ERROR HAPPEND!")
+}
+}
+
+const getUpdateReminderPage = async (req, res) => {
+    try{
+        const reminderId = req.params.id;
+        let [results, fields] = await connection.query(`SELECT * FROM Reminders WHERE id =?`, [reminderId]);
+        let reminder = results && results.length > 0 ? results[0] : {};
+        res.render('editReminder.ejs', {reminderEdit: reminder});
+    }catch(error){
+        console.error(error)
+        res.send("ERROR HAPPEND!")
+}
+}
+
+const postUpdateReminder = async (req, res) => {
+    try{
+        let reminderId = req.body.id;
+        let name = req.body.reminderName;
+        let amount = req.body.amount;
+        let dueDate = req.body.dueDate;
+        let frequency = req.body.frequency;
+        let reminderMethod = req.body.reminderMethod;
+        let reminderNotification = req.body.reminderNotification;
+        console.log(reminderId, name, amount, dueDate, frequency, reminderMethod, reminderNotification);
+        let [results, fields] = await connection.query(`UPDATE Reminders 
+                                                        SET payment_account_name =?, 
+                                                            amount =?, 
+                                                            due_date =?, 
+                                                            frequency =?, 
+                                                            payment_method =?, 
+                                                            notification_time =?
+                                                        WHERE id = ?;`, 
+                                                        [name, amount, dueDate, frequency, reminderMethod, reminderNotification, reminderId]);
+        res.redirect('/view-reminders');
+    }catch(error){
+        console.error(error)
+        res.send("ERROR HAPPEND!")
+}
+}
+
+const postDeleteReminder = async (req, res) => {
+    try {
+        const reminderId = req.body.reminderId;
+        await connection.query("DELETE FROM Reminders WHERE id = ?", [reminderId]);
+        res.redirect("/view-reminders"); 
+    } catch (error) {
+        console.error("Lỗi:", error);
+        res.send("Lỗi khi xóa reminder");
+    }
+}
 
 module.exports = {
     getViewReminders,
     getCreateReminder, 
     getEditReminder,
+    postCreateReminder,
+    postUpdateReminder,
+    getUpdateReminderPage,
+    postDeleteReminder,
+    postDeleteReminder
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -3,7 +3,7 @@ const {getHomePage,} = require('../controllers/homeController')
 const {getLoginPage, postLoginUser,} = require('../controllers/loginController')
 const {getRegisterPage, postCreateUser} = require('../controllers/registerController')
 const {getExpenseStatisticPage,} = require('../controllers/expenseStatistiController')
-const {getViewReminders, getCreateReminder , getEditReminder} = require('../controllers/reminderController')
+const {getViewReminders, getCreateReminder , getEditReminder, postCreateReminder, postUpdateReminder, getUpdateReminderPage, postDeleteReminder} = require('../controllers/reminderController')
 
 const router = express.Router();
 
@@ -24,5 +24,13 @@ router.get('/view-reminders', getViewReminders);
 router.get('/view-reminders/create-reminder', getCreateReminder);
 
 router.get('/view-reminders/edit-reminder', getEditReminder);
+
+router.post('/create-reminder', postCreateReminder);
+
+router.post('/update-reminder', postUpdateReminder);
+
+router.get('/update-reminder/:id', getUpdateReminderPage);
+
+router.post('/delete-reminder', postDeleteReminder);
 
 module.exports = router;
